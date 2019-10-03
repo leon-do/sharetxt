490a006df61786afd2f86a861d1318643f12d90c
Kimmy Monassar <kimmymonassar@gmail.com>
2019-10-03 21:54:31 +0200

    Add stylelint and ESlint

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-const socket = new WebSocket(
-'wss://connect.websocket.in/h4ck3r_m4n?room_id=1989'
-);
+const socket = new WebSocket('wss://connect.websocket.in/h4ck3r_m4n?room_id=1989');
 
 // Connection opened
-socket.addEventListener('open', function(event) {
-document.addEventListener('keyup', e => {
-const text = document.getElementById('txtArea').value;
-socket.send(text);
-});
+socket.addEventListener('open', () => {
+  document.addEventListener('keyup', () => {
+    const text = document.getElementById('txtArea').value;
+    socket.send(text);
+  });
 });
 
 // Listen for messages
-socket.addEventListener('message', function(event) {
-document.getElementById('txtArea').value = event.data;
+socket.addEventListener('message', (event) => {
+  document.getElementById('txtArea').value = event.data;
 });
