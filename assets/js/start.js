0f6cc1dcca0b5b0c45cd7b41be5a3560a622ac64
leon-do <leondo123@gmail.com>
2019-11-20 02:59:20 -0500

    fucki

--- assets/js/start.js
+++ assets/js/start.js
@@ -2,4 +2,4 @@
 const randomId = Math.ceil(Math.random() * 9999)
 
 // create link for button
-$('#startButton').attr('href', `https://${config.url}/id?${randomId}`)
+$('#startButton').attr('href', `/id?${randomId}`)
