2abf97954a8074fead5fc586e28c0a8d09d30160
leon-do <leondo123@gmail.com>
2019-11-21 11:14:20 -0500

    rm socket config and add icon gif

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,14 +1,13 @@
 const config = {
-token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjVlZTM3ZTI1OGNkYzYwMmI5YmZjZmM1MjYyMWQ2N2M0OGVkOTlmYjQ3NTMxMWU3M2E3MTM5OGZlMmViNGVhMWRiMzlkZGVlNWFjNTk0M2I1In0.eyJhdWQiOiI2IiwianRpIjoiNWVlMzdlMjU4Y2RjNjAyYjliZmNmYzUyNjIxZDY3YzQ4ZWQ5OWZiNDc1MzExZTczYTcxMzk4ZmUyZWI0ZWExZGIzOWRkZWU1YWM1OTQzYjUiLCJpYXQiOjE1NzQxOTk3MTYsIm5iZiI6MTU3NDE5OTcxNiwiZXhwIjoxNjA1ODIyMTE2LCJzdWIiOiIzNCIsInNjb3BlcyI6W119.BBDIj2rYKXKhMJckFBSMYPeExYTk0owBILzbdlD_VCBGHqwxUQBNuGmZmPFOQ1fQvjd3yfTVsEceOO4joqWGawkivDs9Ih2QgC9fP0uLpE3W5YvVH-OUvCoqMPBPxwno3Pyfi6hQc_tEBrzwd3Y3cVk2K89RO8tkuHKAPGULB09UkJ3FOQeF-auXX3svvJtYqCCDBHCQ2n5bGjHUXTqMibwJZSB1b0WHiDmBr2XR9FDKtWlRiiuMxHFAJtFS1dMU5v6GYf_kAyEffLRohE0d4WydJSM9tXvXMtQI75Qpa0m6tCTFT40vu8mMXw2E1OcCOFU4Zk9QC-a5ekJ3B0tXuxCTh07YwX8v8Fr3aRWgFXNM4td0TvblcdVubUOyw5hJObcNpY_v0TI6S1KotAhmZ2siaHtKkAGC3kTV2TqM33WfFbsd-t-Y_9Bm_iUPChESwUoCdB1rYL7NcAmq6LFT8z-GTgqa7pkIeVY8g3gu1HnL9ZvAkq7WGttHeZSuvaPoWqgShDfKGMy7FlCno5H2bhXp1O_puYSHqJn9KdCSjrTLry2Y-GrxljWWII-Q7HB7ucY4EEWF38715zAafAEZFPeF2B_ruKwQ8PrPQYdKFQC69B_Wo8t5CQkKDEfYQfCM06EVMjoc2vPbf639ylNr8DWNytvfx437UHrJ7Z9yyVQ',
-url: 'sharetxt.xyz',
-firebase: {
-    apiKey: "AIzaSyDSs1nZUH46tiLN1KpLTTbvdqdsao0f3Fw",
-    authDomain: "sharetxt-21e7c.firebaseapp.com",
-    databaseURL: "https://sharetxt-21e7c.firebaseio.com",
-    projectId: "sharetxt-21e7c",
-    storageBucket: "sharetxt-21e7c.appspot.com",
-    messagingSenderId: "982452079415",
-    appId: "1:982452079415:web:d71500e3581e5c0b5068b2",
-    measurementId: "G-7HP6HYFDZZ"
-}
+    url: 'sharetxt.xyz',
+    firebase: {
+        apiKey: "AIzaSyDSs1nZUH46tiLN1KpLTTbvdqdsao0f3Fw",
+        authDomain: "sharetxt-21e7c.firebaseapp.com",
+        databaseURL: "https://sharetxt-21e7c.firebaseio.com",
+        projectId: "sharetxt-21e7c",
+        storageBucket: "sharetxt-21e7c.appspot.com",
+        messagingSenderId: "982452079415",
+        appId: "1:982452079415:web:d71500e3581e5c0b5068b2",
+        measurementId: "G-7HP6HYFDZZ"
+    }
 }
